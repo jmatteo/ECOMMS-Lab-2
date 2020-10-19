c843834a04c91926db6111c4245672d7b98330f7
Jacob Matteo <matteoj4@students.rowan.edu>
2020-10-19 15:07:18 -0400

    Updated Matlab File
    Added Requirements into file as comments

---
+++ Lab 2.m
@@ -0,0 +1,19 @@
+%%Lab 2 by Jacob Matteo, Jack Henley, and TIm Catrino
+
+%In this project, you will model AM and DSB-SC communications
+%   systems using the Matlab Simulink Communications Systems Toolbox.
+%In both parts, you will test the system with single-tone AM (w
+%   and w/o added Gaussian noise) and multi-tone AM signals (w/o added noise).
+
+
+%The objective is to observe (in time and spectral domains) &
+%   listen as audio tones progresses along a modulation-demodulation
+%   system, both in the presence and absence of added Gaussian
+%   noise (+/- 3dB).
+%Develop a Matlab Simulink simulation of an AM system that includes
+%   a source, modulator, channel, demodulator and sink. Test the
+%   system with single and multi-tone audio frequency message inputs.
+%Note down observations in the time and spectral domains, and listen
+%   to the audio signals and the input and output.
+%Repeat the exercise for a DSB-SC system.
+%Bonus: Demonstrate the Quadrature-Null effect.
